ea99b2763c8123ebcd49c34fb9c85efe906ffa63
JOOuncle <kjhkor90@gmail.com>
2022-09-27 22:12:06 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,11 +12,13 @@ function login() {
     psword: psword.value,
   };
 
-  fetch("/login/", {
+  fetch("/login", {
     method: "POST",
     headers: {
-      "Content-Type": "application.json",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  });
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
 }
