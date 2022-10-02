c6bcb3e20155fc70c8a78a573d5a6e171ced39b9
JOOuncle <kjhkor90@gmail.com>
2022-10-02 15:27:29 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -22,6 +22,18 @@ class UserStorage {
     // console.log(newUsers);
     return newUsers;
   }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userKeys = Object.keys(users); // [>>>.><<<<<id, psword, name]
+    const userInfo = userKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
+  }
 }
 
 module.exports = UserStorage;
