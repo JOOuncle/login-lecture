756c41fe5b6f09091d0301959ab419a462289389
JOOuncle <kjhkor90@gmail.com>
2022-10-06 20:46:21 +0900

    백엔드 맛보기 최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -13,6 +13,7 @@ const output = {
     logger.info(`GET /login "로그인 화면으로 이동"`);
     res.render("home/login");
   },
+
   register: (req, res) => {
     logger.info(`GET /register "회원가입 화면으로 이동"`);
     res.render("home/register");
@@ -23,32 +24,47 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body);
     const response = await user.login();
-    if (response.err)
-      logger.error(
-        `POST /login Response: "success: ${response.success}, ${response.err}" `
-      );
-    else
-      logger.info(
-        `POST /login Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    return res.json(response);
+
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.error ? 400 : 200,
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   },
+
   register: async (req, res) => {
     const user = new User(req.body);
     const response = await user.register();
-    if (response.err)
-      logger.error(
-        `POST /login Response: "success: ${response.success}, ${response.err}" `
-      );
-    else
-      logger.info(
-        `POST /register Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
-    return res.json(response);
+
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.error ? 400 : 200,
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 };
 
 module.exports = {
   output,
   process,
+};
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`
+    );
+  } else {
+    logger.info(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success} ${
+        response.msg || " "
+      }`
+    );
+  }
 };
